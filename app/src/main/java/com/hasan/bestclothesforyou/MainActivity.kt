34a3f760c51256fc921d3e040bebb2a3da929a87
Hasa9nAli <hasan.20cs75@student.uomosul.edu.iq>
2023-04-06 09:44:10 +0300

    request weather Api

--- app/src/main/java/com/hasan/bestclothesforyou/MainActivity.kt
+++ app/src/main/java/com/hasan/bestclothesforyou/MainActivity.kt
@@ -2,10 +2,36 @@ package com.hasan.bestclothesforyou
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hasan.bestclothesforyou.databinding.ActivityMainBinding
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    private val client = OkHttpClient()
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        makeRequestOfWeatherApiOKHTTP()
     }
+
+    private fun makeRequestOfWeatherApiOKHTTP(){
+        val requestWeatherApi = Request.Builder()
+            .url("https://api.tomorrow.io/v4/weather/forecast?apikey=L4sJOkIDWYiG1IRwLPNkjYHgzq3GRgrq&location=egypt")
+            .build()
+        client.newCall(requestWeatherApi).enqueue(object : Callback{
+            override fun onFailure(call: Call, e: IOException) {
+                TODO(e.toString() )
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                runOnUiThread {
+                    binding.textView.text = response.body.toString()
+                }
+            }
+
+        })
+    }
+
 }
