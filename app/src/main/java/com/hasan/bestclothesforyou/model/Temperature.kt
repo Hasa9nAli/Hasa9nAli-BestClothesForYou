80b34134e973045b371e82433213baa919fb50d1
Hasan ALi <hasan.20cs75@student.uomosul.edu.iq>
2023-04-07 11:42:44 +0300

    start working with parse

---
+++ app/src/main/java/com/hasan/bestclothesforyou/model/Temperature.kt
@@ -0,0 +1,8 @@
+package com.hasan.bestclothesforyou.model
+
+
+
+data class StatusWeather(
+    val startTime : String ,
+    val temperature : String
+)
---
+++ app/src/main/java/com/hasan/bestclothesforyou/util/parseJason.kt
@@ -0,0 +1,8 @@
+package com.hasan.bestclothesforyou.util
+
+import org.json.JSONObject
+import java.time.LocalDateTime
+
+fun JSONObject.toTemperature(){
+
+}
