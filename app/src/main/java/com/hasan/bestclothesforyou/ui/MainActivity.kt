f9aaa882b5257f1464af3e30265466c7f900a45d
Hasan ALi <hasan.20cs75@student.uomosul.edu.iq>
2023-04-09 09:03:14 +0300

    complete half home fragment

--- app/src/main/java/com/hasan/bestclothesforyou/ui/MainActivity.kt
+++ app/src/main/java/com/hasan/bestclothesforyou/ui/MainActivity.kt
@@ -2,7 +2,10 @@ package com.hasan.bestclothesforyou.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import com.bumptech.glide.Glide
 import com.google.gson.Gson
 import com.hasan.bestclothesforyou.data.WeatherData
 import com.hasan.bestclothesforyou.databinding.ActivityMainBinding
@@ -10,17 +13,39 @@ import com.hasan.bestclothesforyou.util.Constraint
 import okhttp3.*
 import org.json.JSONObject
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var handler: Handler
     private val client = OkHttpClient()
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        println("test git ")
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        makeRequestOfWeatherApiOKHTTP()
+        handler = Handler(Looper.getMainLooper())
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                // update your view here
+                // for example, if you have a TextView with id "myTextView":
+                binding.textCurrentTime.text = getCurrentTime().removeRange(5, getCurrentTime().length)
+
+                // schedule the task to run again in 1 minute
+                handler.postDelayed(this, 60000)
+                makeRequestOfWeatherApiOKHTTP()
+            }
+        }, 1)
+
     }
+    private fun getCurrentTime(): String {
+        val dateFormat = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
+        val date = Date()
+        return dateFormat.format(date)
+    }
+
 
     private fun makeRequestOfWeatherApiOKHTTP(){
         val requestWeatherApi = Request.Builder()
@@ -33,12 +58,15 @@ class MainActivity : AppCompatActivity() {
 
             override fun onResponse(call: Call, response: Response) {
                 val responseApi = response.body?.string()
+
                 val gson = Gson()
                 val weatherData = gson.fromJson(responseApi, WeatherData::class.java)
-                    runOnUiThread {
-                        binding.textView.text = weatherData.current.observationTime
+                runOnUiThread {
+                        binding.textCurrentTime.text = LocalTime.now().toString().removeRange(5,LocalTime.now().toString().length )
+//                        Glide.with(binding.imageView.context).load(weatherData.current.weatherIcons[0]).into(binding.imageView)
                     }
                 }
+
 
 
         })
