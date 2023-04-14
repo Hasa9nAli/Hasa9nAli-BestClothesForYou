67799be67699dd9e8c6f105e242d92eebe535f15
Hasan ALi <hasan.20cs75@student.uomosul.edu.iq>
2023-04-14 18:14:35 +0300

    upload image from camera and change imageView

--- app/src/main/java/com/hasan/bestclothesforyou/ui/fragment/AddClothesFragment.kt
+++ app/src/main/java/com/hasan/bestclothesforyou/ui/fragment/AddClothesFragment.kt
@@ -1,17 +1,27 @@
 package com.hasan.bestclothesforyou.ui.fragment
 
 import android.R
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import coil.load
 import com.hasan.bestclothesforyou.data.Season
 import com.hasan.bestclothesforyou.databinding.FragmentAddClothesBinding
-import com.google.android.material.button.MaterialButton
 import com.hasan.bestclothesforyou.ui.adapter.SeasonAdapter
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
@@ -32,7 +42,24 @@ class AddClothesFragment : Fragment() {
         binding = FragmentAddClothesBinding.inflate(inflater, container, false)
         binding.recyclerSeason.adapter = seasonAdapter
         createAdapter()
+        binding.imageViewUploadImage.setOnClickListener {
+            cameraCheckPermission()
+        }
+
         return binding.root
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+            val bitmap = data?.extras?.get("data") as Bitmap
+            val sharePreprences = requireActivity().getSharedPreferences("Name", Context.MODE_PRIVATE)
+            val editor = sharePreprences.edit()
+            val imageBitMap = bitmap
+            editor.putString("Name", bitmap.toString())
+            editor.apply()
+            binding.imageViewUploadImage.load(bitmap)
+            binding.iconBack.load(sharePreprences.getString("Name", "testing"))
     }
 
     private fun createAdapter(){
@@ -42,14 +69,7 @@ class AddClothesFragment : Fragment() {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter}
 
-    private companion object{
-        private val  seasons = listOf(
-            Season("winter", false) ,
-            Season("summer", false),
-            Season("autumn", false),
-            Season("spring", false),
-        )
-    }
+
     fun cameraCheckPermission(){
         Dexter.withContext(this.context).withPermissions(
             android.Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -62,20 +82,54 @@ class AddClothesFragment : Fragment() {
                     }
                 }
             }
-
             override fun onPermissionRationaleShouldBeShown(
                 p0: MutableList<PermissionRequest>?,
                 p1: PermissionToken?
             ) {
-                TODO("Not yet implemented")
+                showRotationDialogPermission()
             }
 
-        })
+        }).onSameThread().check()
+    }
+
+    private fun showRotationDialogPermission() {
+        AlertDialog.Builder(this.context).setMessage(
+        "it look like you have turned off permission"+
+        "required for this feature, it can be enable under app setting"
+        )
+            .setPositiveButton("Go To Setting"){_,_->
+
+                    try {   Log.i("AAA", "AAA")
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        Log.i("BBB", "BBB")
+                        val url = Uri.fromParts("package", "com.hasan.bestclothesforyou", null)
+                        Log.i("CCC", "CCC")
+                        intent.data = url
+                        Log.i("DDD", "DDD")
+                        startActivity(intent)
+                        Log.i("EEE", "EEE")
+                    } catch (e: ActivityNotFoundException) {
+                        e.printStackTrace()
+                        Log.i("catch", e.printStackTrace().toString())
+                }
+
+            }.setNegativeButton("Cancle",){dialog,_->
+                run {
+                    dialog.dismiss()
+                }
+            }.show()
     }
 
     private fun camera() {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         startActivityForResult(intent, CAMERA_REQUEST_CODE)
     }
-
+    private companion object{
+        private val  seasons = listOf(
+            Season("winter", false) ,
+            Season("summer", false),
+            Season("autumn", false),
+            Season("spring", false),
+        )
+    }
 }
